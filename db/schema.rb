abb5bcb72f63deab61c6a8cbc5a5efc74b8efba8
3sho7mi8 <hiro.pinot@gmail.com>
2017-05-17 12:20:34 +0900

    Articleモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514072246) do
+ActiveRecord::Schema.define(version: 20170517031519) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
