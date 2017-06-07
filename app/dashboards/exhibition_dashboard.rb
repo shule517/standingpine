aa649b6d410c53969abbe330bdae287ac6073723
3sho7mi8 <hiro.pinot@gmail.com>
2017-06-07 12:43:59 +0900

    ExhibitionのDashboard調整

--- app/dashboards/exhibition_dashboard.rb
+++ app/dashboards/exhibition_dashboard.rb
@@ -31,9 +31,9 @@ class ExhibitionDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :artists,
+    :title_ja,
     :cover_image,
-    :id,
+    :artists,
     :starting_on,
   ].freeze
 
